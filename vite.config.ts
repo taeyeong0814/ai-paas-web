02ae669185fac6a3d5caef91159a279dbea0041b
최기엽 <choikiyeop@gmail.com>
2025-08-14 12:03:00 +0900

    feat: tailwind 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 import path from "path";
 
@@ -7,6 +8,7 @@ import path from "path";
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     svgr({
       include: "**/*.svg",
     }),
