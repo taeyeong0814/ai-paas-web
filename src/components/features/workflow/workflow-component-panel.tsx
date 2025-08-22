ef94f754d617916c98637fc81c8a792372d74a60
최기엽 <choikiyeop@gmail.com>
2025-08-22 09:47:11 +0900

    fix: AccordionTrigger에서 열린 상태의 버튼 스타일 수정

--- src/components/features/workflow/workflow-component-panel.tsx
+++ src/components/features/workflow/workflow-component-panel.tsx
@@ -10,13 +10,13 @@ export const WorkflowComponentPanel = () => {
   const { searchValue, ...restProps } = useSearchInputState();
 
   return (
-    <div className="h-[calc(100vh-98px)] w-[270px] border-r border-gray-200">
-      <div className="flex h-[50px] w-full items-center border-b border-gray-200 bg-white px-4 py-2.5">
+    <div className="flex h-[calc(100vh-98px)] w-[270px] flex-col border-r border-gray-200">
+      <div className="flex h-[50px] w-full flex-shrink-0 items-center border-b border-gray-200 bg-white px-4 py-2.5">
         <div className="truncate text-base font-bold -tracking-[0.5px] text-gray-900">
           테스트 템플릿 001
         </div>
       </div>
-      <div className="px-2.5 pt-2.5 [&>[data-size='m-medium']]:w-full">
+      <div className="flex-shrink-0 px-2.5 pt-2.5 [&>[data-size='m-medium']]:w-full">
         <SearchInput
           variant="default"
           placeholder="검색어를 입력해주세요"
@@ -24,7 +24,7 @@ export const WorkflowComponentPanel = () => {
           {...restProps}
         />
       </div>
-      <div className="flex flex-col gap-[18px] px-4 py-1">
+      <div className="flex min-h-0 flex-1 flex-col gap-[18px] overflow-y-auto px-4 py-1">
         <Accordion type="multiple" className="space-y-1">
           <AccordionItem value="start" className="">
             <AccordionTrigger asChild>
@@ -53,6 +53,10 @@ export const WorkflowComponentPanel = () => {
                 </div>
                 <div className="line-clamp-2">meta-liama/Meta-Liama-3-8B</div>
                 <div className="line-clamp-2">meta-liama/Meta-Liama-3-8B</div>
+                <div className="line-clamp-2">meta-liama/Meta-Liama-3-8B</div>
+                <div className="line-clamp-2">meta-liama/Meta-Liama-3-8B</div>
+                <div className="line-clamp-2">meta-liama/Meta-Liama-3-8B</div>
+                <div className="line-clamp-2">meta-liama/Meta-Liama-3-8B</div>
               </div>
             </AccordionContent>
           </AccordionItem>
@@ -70,6 +74,7 @@ export const WorkflowComponentPanel = () => {
                 </button>
                 <button
                   type="button"
+                  onClick={(e) => e.stopPropagation()}
                   className="relative size-7 rounded border-[#dedede] transition-all duration-300 ease-in-out after:absolute after:top-[48%] after:left-[52%] after:-translate-x-1/2 after:-translate-y-1/2 after:rotate-45 after:text-xl after:font-extralight after:text-[#666] after:content-['×'] hover:border hover:bg-[#f2f2f2] hover:after:text-[#1a1a1a]"
                 >
                   <span className="sr-only">생성</span>
@@ -100,6 +105,7 @@ export const WorkflowComponentPanel = () => {
                 </button>
                 <button
                   type="button"
+                  onClick={(e) => e.stopPropagation()}
                   className="relative size-7 rounded border-[#dedede] transition-all duration-300 ease-in-out after:absolute after:top-[48%] after:left-[52%] after:-translate-x-1/2 after:-translate-y-1/2 after:rotate-45 after:text-xl after:font-extralight after:text-[#666] after:content-['×'] hover:border hover:bg-[#f2f2f2] hover:after:text-[#1a1a1a]"
                 >
                   <span className="sr-only">생성</span>
@@ -130,6 +136,7 @@ export const WorkflowComponentPanel = () => {
                 </button>
                 <button
                   type="button"
+                  onClick={(e) => e.stopPropagation()}
                   className="relative size-7 rounded border-[#dedede] transition-all duration-300 ease-in-out after:absolute after:top-[48%] after:left-[52%] after:-translate-x-1/2 after:-translate-y-1/2 after:rotate-45 after:text-xl after:font-extralight after:text-[#666] after:content-['×'] hover:border hover:bg-[#f2f2f2] hover:after:text-[#1a1a1a]"
                 >
                   <span className="sr-only">생성</span>
