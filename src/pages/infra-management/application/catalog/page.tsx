d239b27238d16565b8bd2713fb40d370710ee352
최기엽 <choikiyeop@gmail.com>
2025-10-15 12:30:31 +0900

    feat: 애플리케이션 카탈로그 페이지에 이미지 및 스타일 추가

--- src/pages/infra-management/application/catalog/page.tsx
+++ src/pages/infra-management/application/catalog/page.tsx
@@ -1,8 +1,431 @@
+import { useState } from 'react';
+import {
+  BreadCrumb,
+  Button,
+  Pagination,
+  SearchInput,
+  Select,
+  useSearchInputState,
+  type SelectSingleValue,
+} from '@innogrid/ui';
+
+import ImgCatalog1 from '../../../../assets/img/catalog/img_catalog1.png';
+import styles from '../../inframonitor.module.scss';
+
+//breadcrumb
+const items = [
+  { label: '인프라 모니터', path: '/infra-monitor' },
+  { label: '애플리케이션' },
+  { label: '카탈로그' },
+];
+
+//select option
+type OptionType = { text: string; value: string };
+
+const options = [
+  { text: '옵션 1', value: 'option1' },
+  { text: '옵션 2', value: 'option2' },
+  { text: '옵션 3', value: 'option3' },
+];
+
 export default function ApplicationCatalogPage() {
+  const [selectedValue, setSelectedValue] = useState<OptionType>();
+
+  const onChangeSelect = (option: SelectSingleValue<OptionType>) => {
+    setSelectedValue(option);
+  };
+
+  //SearchInput
+  const { searchValue, ...restProps } = useSearchInputState();
+
+  //pagination
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(10);
+  const totalCount = 100;
+  const pageSizeOption = [10, 15, 20, 30, 50, 100, 500];
+
   return (
-    <div className="p-6">
-      <h1 className="mb-4 text-2xl font-bold">애플리케이션 카탈로그</h1>
-      <p>애플리케이션 카탈로그 페이지입니다.</p>
-    </div>
+    <main>
+      <BreadCrumb items={items} onNavigate={(path: string) => {}} className="breadcrumbBox" />
+      <div className="page-title-box">
+        <h2 className="page-title">카탈로그</h2>
+      </div>
+      <div className="page-content">
+        <div className={styles.flexBox}>
+          <Select
+            className="page-input_item-data_select"
+            options={options}
+            getOptionLabel={(option) => option.text}
+            getOptionValue={(option) => option.value}
+            value={selectedValue}
+            onChange={onChangeSelect}
+            size="m-small"
+          />
+          <SearchInput size="m-medium" placeholder="검색어를 입력해주세요" {...restProps} />
+        </div>
+        <div className="page-content-detail-row2 page-mt-24 flex-wrap">
+          <div className={styles.catalogBox}>
+            <div className={styles.catalogInfo}>
+              <div className={styles.catalogImg}>
+                <img src={ImgCatalog1} alt="ImgCatalog1" height="84px" width="84px" />
+              </div>
+              <div className={styles.catalogDesc}>
+                <div>
+                  <div className={styles.catalogTit}>
+                    <p>Jenkins</p>
+                    <span>5.0.13</span>
+                  </div>
+                  <div className={styles.catalogTxt}>
+                    Jenkins - Build great things at any scale! The leading open source automation
+                    server, Jenkins provides over 1800 plugins to support building, deploying and
+                    automating any project. Build great things at any scale! The leading open source
+                    automation server, Jenkins provides over 1800 plugins to support building,
+                    deploying and automating any project.
+                  </div>
+                </div>
+                <div className={styles.catalogKeyword}>
+                  <div className={styles.catalogKeywordDetail}>
+                    <span>Jenkins</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                  </div>
+                  <button type="button" className={styles.btnKeywordNum}>
+                    +5
+                  </button>
+                  <div className={styles.keywordNumList}>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.catalogBtns}>
+              <Button onClick={() => alert('Button clicked!')} color="secondary">
+                상세 정보
+              </Button>
+              <Button onClick={() => alert('Button clicked!')} color="focus">
+                헬름 배포
+              </Button>
+            </div>
+          </div>
+          <div className={styles.catalogBox}>
+            <div className={styles.catalogInfo}>
+              <div className={styles.catalogImg}>
+                <img src={ImgCatalog1} alt="ImgCatalog1" height="84px" width="84px" />
+              </div>
+              <div className={styles.catalogDesc}>
+                <div>
+                  <div className={styles.catalogTit}>
+                    <p>Jenkins</p>
+                    <span>5.0.13</span>
+                  </div>
+                  <div className={styles.catalogTxt}>
+                    Jenkins - Build great things at any scale! The leading open source automation
+                    server, Jenkins provides over 1800 plugins to support building, deploying and
+                    automating any project.
+                  </div>
+                </div>
+                <div className={styles.catalogKeyword}>
+                  <div className={styles.catalogKeywordDetail}></div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.catalogBtns}>
+              <Button onClick={() => alert('Button clicked!')} color="secondary">
+                상세 정보
+              </Button>
+              <Button onClick={() => alert('Button clicked!')} color="focus">
+                헬름 배포
+              </Button>
+            </div>
+          </div>
+          <div className={styles.catalogBox}>
+            <div className={styles.catalogInfo}>
+              <div className={styles.catalogImg}>
+                <img src={ImgCatalog1} alt="ImgCatalog1" height="84px" width="84px" />
+              </div>
+              <div className={styles.catalogDesc}>
+                <div>
+                  <div className={styles.catalogTit}>
+                    <p>Jenkins</p>
+                    <span>5.0.13</span>
+                  </div>
+                  <div className={styles.catalogTxt}>
+                    Jenkins - Build great things at any scale! The leading open source automation
+                    server, Jenkins provides over 1800 plugins to support building, deploying and
+                    automating any project. Build great things at any scale! The leading open source
+                    automation server, Jenkins provides over 1800 plugins to support building,
+                    deploying and automating any project.
+                  </div>
+                </div>
+                <div className={styles.catalogKeyword}>
+                  <div className={styles.catalogKeywordDetail}>
+                    <span>Jenkins</span>
+                    <span>Keyword</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.catalogBtns}>
+              <Button onClick={() => alert('Button clicked!')} color="secondary">
+                상세 정보
+              </Button>
+              <Button onClick={() => alert('Button clicked!')} color="focus">
+                헬름 배포
+              </Button>
+            </div>
+          </div>
+          <div className={styles.catalogBox}>
+            <div className={styles.catalogInfo}>
+              <div className={styles.catalogImg}>
+                <img src={ImgCatalog1} alt="ImgCatalog1" height="84px" width="84px" />
+              </div>
+              <div className={styles.catalogDesc}>
+                <div>
+                  <div className={styles.catalogTit}>
+                    <p>Jenkins</p>
+                    <span>5.0.13</span>
+                  </div>
+                  <div className={styles.catalogTxt}>
+                    Jenkins - Build great things at any scale! The leading open source automation
+                    server, Jenkins provides over 1800 plugins to support building, deploying and
+                    automating any project. Build great things at any scale! The leading open source
+                    automation server, Jenkins provides over 1800 plugins to support building,
+                    deploying and automating any project.
+                  </div>
+                </div>
+                <div className={styles.catalogKeyword}>
+                  <div className={styles.catalogKeywordDetail}>
+                    <span>Jenkins</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                  </div>
+                  <button type="button" className={styles.btnKeywordNum}>
+                    +5
+                  </button>
+                  <div className={styles.keywordNumList}>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.catalogBtns}>
+              <Button onClick={() => alert('Button clicked!')} color="secondary">
+                상세 정보
+              </Button>
+              <Button onClick={() => alert('Button clicked!')} color="focus">
+                헬름 배포
+              </Button>
+            </div>
+          </div>
+          <div className={styles.catalogBox}>
+            <div className={styles.catalogInfo}>
+              <div className={styles.catalogImg}>
+                <img src={ImgCatalog1} alt="ImgCatalog1" height="84px" width="84px" />
+              </div>
+              <div className={styles.catalogDesc}>
+                <div>
+                  <div className={styles.catalogTit}>
+                    <p>Jenkins</p>
+                    <span>5.0.13</span>
+                  </div>
+                  <div className={styles.catalogTxt}>
+                    Jenkins - Build great things at any scale! The leading open source automation
+                    server, Jenkins provides over 1800 plugins to support building, deploying and
+                    automating any project.
+                  </div>
+                </div>
+                <div className={styles.catalogKeyword}>
+                  <div className={styles.catalogKeywordDetail}></div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.catalogBtns}>
+              <Button onClick={() => alert('Button clicked!')} color="secondary">
+                상세 정보
+              </Button>
+              <Button onClick={() => alert('Button clicked!')} color="focus">
+                헬름 배포
+              </Button>
+            </div>
+          </div>
+          <div className={styles.catalogBox}>
+            <div className={styles.catalogInfo}>
+              <div className={styles.catalogImg}>
+                <img src={ImgCatalog1} alt="ImgCatalog1" height="84px" width="84px" />
+              </div>
+              <div className={styles.catalogDesc}>
+                <div>
+                  <div className={styles.catalogTit}>
+                    <p>Jenkins</p>
+                    <span>5.0.13</span>
+                  </div>
+                  <div className={styles.catalogTxt}>
+                    Jenkins - Build great things at any scale! The leading open source automation
+                    server, Jenkins provides over 1800 plugins to support building, deploying and
+                    automating any project. Build great things at any scale! The leading open source
+                    automation server, Jenkins provides over 1800 plugins to support building,
+                    deploying and automating any project.
+                  </div>
+                </div>
+                <div className={styles.catalogKeyword}>
+                  <div className={styles.catalogKeywordDetail}>
+                    <span>Jenkins</span>
+                    <span>Keyword</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.catalogBtns}>
+              <Button onClick={() => alert('Button clicked!')} color="secondary">
+                상세 정보
+              </Button>
+              <Button onClick={() => alert('Button clicked!')} color="focus">
+                헬름 배포
+              </Button>
+            </div>
+          </div>
+          <div className={styles.catalogBox}>
+            <div className={styles.catalogInfo}>
+              <div className={styles.catalogImg}>
+                <img src={ImgCatalog1} alt="ImgCatalog1" height="84px" width="84px" />
+              </div>
+              <div className={styles.catalogDesc}>
+                <div>
+                  <div className={styles.catalogTit}>
+                    <p>Jenkins</p>
+                    <span>5.0.13</span>
+                  </div>
+                  <div className={styles.catalogTxt}>
+                    Jenkins - Build great things at any scale! The leading open source automation
+                    server, Jenkins provides over 1800 plugins to support building, deploying and
+                    automating any project. Build great things at any scale! The leading open source
+                    automation server, Jenkins provides over 1800 plugins to support building,
+                    deploying and automating any project.
+                  </div>
+                </div>
+                <div className={styles.catalogKeyword}>
+                  <div className={styles.catalogKeywordDetail}>
+                    <span>Jenkins</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                  </div>
+                  <button type="button" className={styles.btnKeywordNum}>
+                    +5
+                  </button>
+                  <div className={styles.keywordNumList}>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                    <span>Keyword</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.catalogBtns}>
+              <Button onClick={() => alert('Button clicked!')} color="secondary">
+                상세 정보
+              </Button>
+              <Button onClick={() => alert('Button clicked!')} color="focus">
+                헬름 배포
+              </Button>
+            </div>
+          </div>
+          <div className={styles.catalogBox}>
+            <div className={styles.catalogInfo}>
+              <div className={styles.catalogImg}>
+                <img src={ImgCatalog1} alt="ImgCatalog1" height="84px" width="84px" />
+              </div>
+              <div className={styles.catalogDesc}>
+                <div>
+                  <div className={styles.catalogTit}>
+                    <p>Jenkins</p>
+                    <span>5.0.13</span>
+                  </div>
+                  <div className={styles.catalogTxt}>
+                    Jenkins - Build great things at any scale! The leading open source automation
+                    server, Jenkins provides over 1800 plugins to support building, deploying and
+                    automating any project.
+                  </div>
+                </div>
+                <div className={styles.catalogKeyword}>
+                  <div className={styles.catalogKeywordDetail}></div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.catalogBtns}>
+              <Button onClick={() => alert('Button clicked!')} color="secondary">
+                상세 정보
+              </Button>
+              <Button onClick={() => alert('Button clicked!')} color="focus">
+                헬름 배포
+              </Button>
+            </div>
+          </div>
+          <div className={styles.catalogBox}>
+            <div className={styles.catalogInfo}>
+              <div className={styles.catalogImg}>
+                <img src={ImgCatalog1} alt="ImgCatalog1" height="84px" width="84px" />
+              </div>
+              <div className={styles.catalogDesc}>
+                <div>
+                  <div className={styles.catalogTit}>
+                    <p>Jenkins</p>
+                    <span>5.0.13</span>
+                  </div>
+                  <div className={styles.catalogTxt}>
+                    Jenkins - Build great things at any scale! The leading open source automation
+                    server, Jenkins provides over 1800 plugins to support building, deploying and
+                    automating any project. Build great things at any scale! The leading open source
+                    automation server, Jenkins provides over 1800 plugins to support building,
+                    deploying and automating any project.
+                  </div>
+                </div>
+                <div className={styles.catalogKeyword}>
+                  <div className={styles.catalogKeywordDetail}>
+                    <span>Jenkins</span>
+                    <span>Keyword</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.catalogBtns}>
+              <Button onClick={() => alert('Button clicked!')} color="secondary">
+                상세 정보
+              </Button>
+              <Button onClick={() => alert('Button clicked!')} color="focus">
+                헬름 배포
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.paginationBox}>
+          <Pagination
+            page={page}
+            pageSizeOption={pageSizeOption}
+            size={size}
+            totalCount={totalCount}
+            onChangePageInput={(event) => setPage(+event.target.value)}
+            onChangePageSize={(event) => setSize(+event.target.value)}
+            onClickNext={() => setPage(page + 1)}
+            onClickPrev={() => setPage(page - 1)}
+          />
+        </div>
+      </div>
+    </main>
   );
 }
